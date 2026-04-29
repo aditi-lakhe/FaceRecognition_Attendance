55a855c7e8143974ecff1b6b0a3144d2a305867d
Tanvi Kondedeshmukh <tanvikondedeshmukh2008@gmail.com>
2026-04-29 23:17:46 +0530

    files added

---
+++ proj3.py
@@ -0,0 +1,21 @@
+import tkinter as tk
+import FaceRecognition_Attendance.util as util
+
+class App:
+    def __init__(self):
+        self.main_window = tk.Tk()
+        self.main_window.geometry("1200x520+350+100")
+        self.login_button_main_window=util.get_button(self.main_window,"Login","green",self.login)
+        self.register_new_user_button_main_window=util.get_button(self.main_window,"Register New User","gray",self.register_new_user,fg='black')
+        self.register_new_user_button_main_window.place(x=750,y=400)
+        self.webcam_label=util.get_img_label(self.main_window)
+        self.webcam_label.place(x=10,y=0,width=700,height=500)
+        self.add_webcam(self.webcam_label)
+        pass
+    def start(self):
+        pass
+
+if __name__ == "__main__":
+    app = App()
+    app.start()
+
